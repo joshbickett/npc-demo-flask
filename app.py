544ae1876ce6f9668b7e7a6542f04faf7e8a7806
Josh Bickett <josh@bickett.net>
2023-08-16 22:48:39 -0700

    Add `load_dotenv` and update `requirements.txt`

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
 """
 The main app
 """
+import os
 from flask import Flask, request, jsonify
 import openai
 from flask_cors import CORS
+from dotenv import load_dotenv
 from prompts import get_character_prompt
+
+
+load_dotenv()
 
 
 app = Flask(__name__)
 CORS(app)
+openai.api_key = os.environ.get('OPENAI_API_KEY')
 
 @app.route('/')
 def index():
